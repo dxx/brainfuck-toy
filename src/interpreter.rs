3b045e24f3e457880777f4f6d41b8f5d64a6f9da
dxx <dx1898@foxmail.com>
2022-03-29 04:47:32 +0100

    Modify interpreter

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,6 +1,67 @@
-use super::{opcode, opcode::Opcode};
+use super::opcode;
 
+use std::collections;
 use std::io::prelude::*;
+
+#[derive(Debug, PartialEq, Eq)]
+enum Opcode {
+    SHL,
+    SHR,
+    ADD,
+    SUB,
+    LSB,
+    RSB,
+    GETCHAR,
+    PUTCHAR,
+}
+
+impl From<u8> for Opcode {
+    fn from(u: u8) -> Self {
+        match u {
+            opcode::OPCODE_SHL => Opcode::SHL,
+            opcode::OPCODE_SHR => Opcode::SHR,
+            opcode::OPCODE_ADD => Opcode::ADD,
+            opcode::OPCODE_SUB => Opcode::SUB,
+            opcode::OPCODE_LSB => Opcode::LSB,
+            opcode::OPCODE_RSB => Opcode::RSB,
+            opcode::OPCODE_GETCHAR => Opcode::GETCHAR,
+            opcode::OPCODE_PUTCHAR => Opcode::PUTCHAR,
+            _ => panic!("Unsupported opcode {}", u),
+        }
+    }
+}
+
+struct Code {
+    opcodes: Vec<Opcode>,
+    jump_table: collections::HashMap<usize, usize>,
+}
+
+impl Code {
+    fn from(data: Vec<u8>) -> Result<Self, Box<dyn std::error::Error>> {
+        let opcodes: Vec<Opcode> = data.iter()
+            .filter(|x| opcode::OPCODE_DICT.contains(x))
+            .map(|x| Opcode::from(*x))
+            .collect();
+
+        let mut jump_table = collections::HashMap::new();
+        let mut stack = Vec::new();
+        
+        for (i, v) in opcodes.iter().enumerate() {
+            if Opcode::LSB == *v {
+                stack.push(i);
+            }
+            if Opcode::RSB == *v {
+                let j = stack.pop().ok_or("Pop from empty stack")?;
+                // 保存 [ 和 ] 程序计数器的对应关系
+                jump_table.insert(j, i);
+                jump_table.insert(i, j);
+            }
+        }
+ 
+        Ok(Code { opcodes, jump_table })
+    }
+
+}
 
 /// 解释器
 pub struct Interpreter {
@@ -14,12 +75,11 @@ impl std::default::Default for Interpreter {
 }
 
 impl Interpreter {
-
     pub fn run(&mut self, data: Vec<u8>) -> Result<(), Box<dyn std::error::Error>> {
-        let code = opcode::Code::from(data)?;
-        let opcodes = code.opcodes();
+        let code = Code::from(data)?;
+        let opcodes = code.opcodes;
         let opcode_len = opcodes.len();
-        let jump_table = code.jump_table();
+        let jump_table = code.jump_table;
         let mut pc = 0; // 程序计数器
         let mut s_pointer = 0; // 指针
         
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,17 @@
 pub mod opcode;
 pub mod interpreter;
+pub mod interpreter_it;
+
+use std::io::prelude::*;
+
+pub fn parse_data() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
+
+    assert!(args.len() >= 2);
+
+    let mut f = std::fs::File::open(&args[1])?;
+    let mut data: Vec<u8> = Vec::new();
+    f.read_to_end(&mut data)?;
+
+    Ok(data)
+}
