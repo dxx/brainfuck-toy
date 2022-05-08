275ecb6611b0848a7677c75f1c99b49772a856b6
dxx <dx1898@foxmail.com>
2022-05-08 10:05:34 +0800

    Use once cell

--- src/opcode.rs
+++ src/opcode.rs
@@ -1,4 +1,4 @@
-use lazy_static::lazy_static;
+use once_cell::sync::Lazy;
 
 pub const OPCODE_SHL: u8     = b'<'; // <: 指针减一
 pub const OPCODE_SHR: u8     = b'>'; // >: 指针加一
@@ -9,15 +9,13 @@ pub const OPCODE_RSB: u8     = b']'; // ]: 如果指针指向的单元值不为�
 pub const OPCODE_GETCHAR: u8 = b','; // ,: 输入内容到指针指向的单元（ASCII码）
 pub const OPCODE_PUTCHAR: u8 = b'.'; // .: 输出指针指向的单元内容（ASCII码）
 
-lazy_static! {
-    pub static ref OPCODE_DICT: Vec<u8> = vec![
-        OPCODE_SHL,
-        OPCODE_SHR,
-        OPCODE_ADD,
-        OPCODE_SUB,
-        OPCODE_LSB,
-        OPCODE_RSB,
-        OPCODE_GETCHAR,
-        OPCODE_PUTCHAR,
-    ];
-}
+pub static OPCODE_DICT: Lazy<Vec<u8>> = Lazy::new(|| vec![
+    OPCODE_SHL,
+    OPCODE_SHR,
+    OPCODE_ADD,
+    OPCODE_SUB,
+    OPCODE_LSB,
+    OPCODE_RSB,
+    OPCODE_GETCHAR,
+    OPCODE_PUTCHAR,
+]);
